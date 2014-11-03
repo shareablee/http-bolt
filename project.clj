1c78bf613a1506d2ef2e7f97b5186ed26076400a
Jeremy Heiler <jeremyheiler@gmail.com>
2014-11-03 16:04:19 -0500

    Update collection common from 0.1.5-SNAPSHOT to 0.1.5 proper

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject http-bolt "0.1.0-SNAPSHOT"
   :description "A bolt for making arbitrary HTTP requests."
-  :dependencies [[com.shareablee/collection "0.1.5-SNAPSHOT"]
+  :dependencies [[com.shareablee/collection "0.1.5"]
                  [org.clojure/clojure "1.4.0"]
                  [clj-http "0.9.2" :exclusions [cheshire]]
                  [storm "0.9.0.1" :exclusions [org.apache.httpcomponents/httpclient]]]
